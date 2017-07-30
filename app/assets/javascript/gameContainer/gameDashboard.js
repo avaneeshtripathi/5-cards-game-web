641a5edd95d4d211c2ac1f54acfbedcaf5c5bfda
avaneesh-webonise <avaneesh.tripathi@weboniselab.com>
2017-07-31 00:23:27 +0530

    krakened images

--- app/assets/javascript/gameContainer/gameDashboard.js
+++ app/assets/javascript/gameContainer/gameDashboard.js
@@ -11,26 +11,11 @@ export default class GameDashboard extends React.Component {
     render () {
         return (
             <ul className="gameListWrapper row">
-                <li className="col-lg-3 col-md-4 col-sm-6">
+                <li className="col-lg-3 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
                     <div className="gameListCard fiveCardsGameCard">
                         <span className="activeLabel">ACTIVE</span>
                         <h2 className="text-center">5 Cards Game</h2>
                         <div className="playNowOverlay text-center"><button onClick={this.startGame.bind(this, 'fiveCardsGame')}>Play Now</button></div>
-                    </div>
-                </li>
-                <li className="col-lg-3 col-md-4 col-sm-6">
-                    <div className="gameListCard">
-                        <h2 className="text-center">NA</h2>
-                    </div>
-                </li>
-                <li className="col-lg-3 col-md-4 col-sm-6">
-                    <div className="gameListCard">
-                        <h2 className="text-center">NA</h2>
-                    </div>
-                </li>
-                <li className="col-lg-3 col-md-4 col-sm-6">
-                    <div className="gameListCard">
-                        <h2 className="text-center">NA</h2>
                     </div>
                 </li>
             </ul>
